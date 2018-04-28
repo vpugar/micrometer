75a022351e845975ba53ef329c862eba9d3b7783
Vedran Pugar <vedran.pugar@kapsch.net>
2018-04-28 22:36:56 +0200

    Performance test with big number of registered counters

--- micrometer-core/src/test/java/io/micrometer/core/instrument/MeterRegistryPerfTest.java
+++ micrometer-core/src/test/java/io/micrometer/core/instrument/MeterRegistryPerfTest.java
@@ -5,12 +5,32 @@ import org.junit.jupiter.api.Test;
 import org.springframework.util.StopWatch;
 
 public class MeterRegistryPerfTest {
+
     private MeterRegistry registry = new SimpleMeterRegistry();
 
     @Test
-    public void test() {
+    public void test20000() {
 
-        StopWatch stopWatch = new StopWatch();
+        StopWatch stopWatch = new StopWatch("20000");
+
+        // worming
+        for (int i = 0; i < 1000; i++) {
+            registry.counter("my.counter", "w" + i, "v1");
+        }
+
+        stopWatch.start();
+        for (int i = 0; i < 20000; i++) {
+            registry.counter("my.counter", "k" + i, "v1");
+        }
+        stopWatch.stop();
+
+        System.out.println(stopWatch.prettyPrint());
+    }
+
+    @Test
+    public void test100000() {
+
+        StopWatch stopWatch = new StopWatch("100000");
 
         // worming
         for (int i = 0; i < 1000; i++) {
@@ -26,4 +46,35 @@ public class MeterRegistryPerfTest {
         System.out.println(stopWatch.prettyPrint());
     }
 
+    @Test
+    public void testGet() {
+
+        StopWatch stopWatch = new StopWatch("getMeters 20000");
+
+        for (int i = 0; i < 20000; i++) {
+            registry.counter("my.counter", "k" + i, "v1");
+        }
+
+        stopWatch.start();
+        registry.getMeters();
+        stopWatch.stop();
+
+        System.out.println(stopWatch.prettyPrint());
+    }
+
+    @Test
+    public void testGetOne() {
+
+        StopWatch stopWatch = new StopWatch("get one - k5000");
+
+        for (int i = 0; i < 20000; i++) {
+            registry.counter("my.counter", "k" + i, "v1");
+        }
+
+        stopWatch.start();
+        registry.get("k5000");
+        stopWatch.stop();
+
+        System.out.println(stopWatch.prettyPrint());
+    }
 }
